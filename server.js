ec8f44ae44010d5547f94b592ea73c166e2a5b94
Harsh Mahajan <INT_HarshM@microland.com>
2019-05-23 17:47:59 +0530

    Add error handling part 5

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer(app);
 server.listen(port);
+
+/*
+    npm install --save-dev nodemon
+    npm install --save morgan
+    npm install body-parser
+*/
