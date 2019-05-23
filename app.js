076b89ac371d3db6839c1f7db74d78dbb005deb9
Harsh Mahajan <harshmahajan927@yahoo.com>
2019-05-23 23:48:56 +0530

    Add post product feature

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const productsRoute = require('./api/routes/product')
+const productsRoute = require('./api/routes/product-route')
+
+mongoose.connect(
+    'mongodb+srv://admin:' + process.env.MONGO_ATLAS_PASSWORD + '@rest-shop-6hkxp.mongodb.net/test?retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization');
+    if(req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
+        return res.status(200).json({});
+    }
+    next();
+})
 
 app.use('/products', productsRoute);
 
--- server.js
+++ server.js
@@ -10,4 +10,8 @@ server.listen(port);
     npm install --save-dev nodemon
     npm install --save morgan
     npm install body-parser
+    npm install --save mongoose
+    
+    Dd9bLAjLZD5mYBA!
+    admin :: admin
 */
