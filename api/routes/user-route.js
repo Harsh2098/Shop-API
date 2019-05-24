333b20527f525873e941073145c33ba77a2a4e7b
Harsh Mahajan <harshmahajan927@yahoo.com>
2019-05-24 17:36:13 +0530

    Add user login feature

--- api/routes/user-route.js
+++ api/routes/user-route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const User = require("../models/user");
 const mongoose = require("mongoose");
@@ -51,6 +52,44 @@ router.post("/signup", (req, res, next) => {
         error: err
       });
     });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+  .exec()
+  .then(user => {
+    if(user) {
+      bcrypt.compare(req.body.password, user.password, (err, match) => {
+        if(err) return req.status(500).json({message: "Authentication failed"});
+
+        if(match) {
+          const token = jwt.sign({
+            email: user.email,
+            userId: user._id
+          },
+          process.env.JWT_KEY, {
+            expiresIn: '1h'
+          });
+
+          return res.status(200).json({
+            message: 'Authentication successful',
+            token: token
+          });
+        }
+
+        return res.status(500).json({message: "Authentication failed"});
+      });
+
+    } else {
+      console.log(user);
+      return res.status(404).json({
+        message: 'Authentication failed'
+      });
+    }
+  })
+  .catch(err => {
+    console.log({error: err});
+  })
 });
 
 module.exports = router;
